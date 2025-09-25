ddc71f8d1e7a7326de095bde6e3ba6b2f8e21922
rhaihanadzani <mrhaihan17@gmail.com>
2025-09-25 11:05:40 +0700

    semoga ini bisa

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,22 +6,25 @@ import RulesSection from "@/components/RulesSection";
 import AwardSections from "@/components/Award";
 import LoginModalHome from "@/components/LoginModalHome";
 import { fontWorkSans } from "@/lib/fonts";
+import { Suspense } from "react";
 
 export default function Home() {
   const font3 = fontWorkSans.className;
 
   return (
-    <main className={`min-h-screen font-sans ${font3}`}>
-      <LoginModalHome />
+    <Suspense fallback={<div>Loading...</div>}>
+      <main className={`min-h-screen font-sans ${font3}`}>
+        <LoginModalHome />
 
-      {/* Sections */}
-      {/* <HeroSection /> */}
-      <AboutSection />
-      <CollectionSection />
-      <FacilitiesSection />
-      <AwardSections />
-      <RulesSection />
-      <Footer />
-    </main>
+        {/* Sections */}
+        {/* <HeroSection /> */}
+        <AboutSection />
+        <CollectionSection />
+        <FacilitiesSection />
+        <AwardSections />
+        <RulesSection />
+        <Footer />
+      </main>
+    </Suspense>
   );
 }
