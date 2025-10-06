bfcefe54d87ac6b7c1ce7f4a33cf6c44a0c17d73
rhaihanadzani <mrhaihan17@gmail.com>
2025-10-06 14:53:39 +0700

    add google analitics

--- src/components/layouts/shell/app-layout.tsx
+++ src/components/layouts/shell/app-layout.tsx
@@ -4,15 +4,37 @@ import { Toaster } from "@/components/ui/sonner";
 // import { Toaster } from "@/components/ui/toaster";
 import BottomNav from "./bottom-nav";
 import TopNav from "./top-nav";
+import Script from "next/script";
 
 const AppLayout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <div>
-      <Toaster position="top-right" />
-      <TopNav />
-      {children}
-      <BottomNav />
-    </div>
+    <>
+      {/* Google Analytics */}
+      {process.env.NEXT_PUBLIC_GA_ID && (
+        <>
+          <Script
+            src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_ID}`}
+            strategy="afterInteractive"
+          />
+          <Script id="google-analytics" strategy="afterInteractive">
+            {`
+                window.dataLayer = window.dataLayer || [];
+                function gtag(){dataLayer.push(arguments);}
+                gtag('js', new Date());
+                gtag('config', '${process.env.NEXT_PUBLIC_GA_ID}', {
+                  page_path: window.location.pathname,
+                });
+              `}
+          </Script>
+        </>
+      )}
+      <div>
+        <Toaster position="top-right" />
+        <TopNav />
+        {children}
+        <BottomNav />
+      </div>
+    </>
   );
 };
 export default AppLayout;
